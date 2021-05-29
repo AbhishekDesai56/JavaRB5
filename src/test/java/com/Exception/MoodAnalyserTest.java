9349fc35c44767539adb878b948659e845f885f8
AbhishekDesai56 <abhishekdesai56@gmail.com>
2021-05-29 22:02:11 +0530

    Created Function that analyse the mood

--- src/test/java/com/Exception/MoodAnalyserTest.java
+++ src/test/java/com/Exception/MoodAnalyserTest.java
@@ -1,4 +1,20 @@
 package com.Exception;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class MoodAnalyserTest {
+    @Test
+    public void givenMessageShouldReturnSad() {
+        MoodAnalyser moodAnalyser = new MoodAnalyser();
+        String mood = moodAnalyser.analyseMood("I am in Sad Mood");
+        Assertions.assertEquals("SAD", mood);
+    }
+
+    @Test
+    void givenMessageShouldReturnHappy() {
+        MoodAnalyser moodAnalyser = new MoodAnalyser();
+        String mood = moodAnalyser.analyseMood("I am in Any Mood");
+        Assertions.assertEquals("HAPPY", mood);
+    }
 }
